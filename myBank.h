16df8dd43d55c7fd412c29a5bfcedaa1cd0df4bd
ohadcohen1111 <57725202+ohadcohen1111@users.noreply.github.com>
2019-12-01 15:04:29 +0200

    Create myBank.h

---
+++ myBank.h
@@ -0,0 +1,9 @@
+void open(double bank[2][50]);
+void balance(double bank[2][50]);
+void deposit(double bank[2][50]);
+void withdrawal(double bank[2][50]);
+void close(double bank[2][50]);
+void interest(double bank[2][50]);
+void print(double bank[2][50]);
+void exit_program(double bank[2][50]);
+int main();
